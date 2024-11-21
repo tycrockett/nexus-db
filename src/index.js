8134984e38d2bdb69603e0510b8fa48b432ae64e
tycrockett <ty.crockett53@gmail.com>
2024-11-21 11:46:39 -0700

    Update package

---
+++ src/index.js
@@ -0,0 +1,153 @@
+import { useEffect, useRef, useState } from "react";
+import * as uuid from "uuid";
+
+const createSelector = (path) => {
+  const selector = (obj) => path.reduce((acc, key) => acc?.[key], obj);
+  selector.path = path;
+  selector.id = uuid.v4();
+  selector.pathKey = path.join(".");
+  return selector;
+};
+
+const dataSetter = (object, selector, newValue) => {
+  const path = selector.path;
+  if (!path) {
+    throw new Error('Selector must have a "path" property for setting.');
+  }
+  let current = object;
+  for (let i = 0; i < path.length - 1; i++) {
+    current = current[path[i]];
+  }
+  current[path[path.length - 1]] = newValue;
+};
+
+class ListenerTree {
+  constructor() {
+    this.tree = {};
+  }
+
+  add(path, callback) {
+    let node = this.tree;
+    for (const key of path) {
+      if (!node[key]) {
+        node[key] = { __listeners: [] };
+      }
+      node = node[key];
+    }
+    node.__listeners.push(callback);
+  }
+
+  remove(path, callback) {
+    let node = this.tree;
+    const stack = [];
+    for (const key of path) {
+      if (!node[key]) return; // Path doesn't exist
+      stack.push([node, key]);
+      node = node[key];
+    }
+    node.__listeners = node.__listeners.filter((cb) => cb !== callback);
+
+    // Clean up empty nodes
+    while (stack.length) {
+      const [parent, key] = stack.pop();
+      if (
+        Object.keys(parent[key]).length === 1 &&
+        parent[key].__listeners.length === 0
+      ) {
+        delete parent[key];
+      } else {
+        break;
+      }
+    }
+  }
+
+  notify(path) {
+    const notifyCallbacks = (node) => {
+      if (!node) return;
+      (node.__listeners || []).forEach((cb) => cb(Date.now()));
+      for (const key of Object.keys(node)) {
+        if (key !== "__listeners") notifyCallbacks(node[key]);
+      }
+    };
+
+    // Notify exact path
+    let node = this.tree;
+    for (const key of path) {
+      if (!node[key]) break;
+      node = node[key];
+    }
+    notifyCallbacks(node);
+
+    // Notify parent paths
+    let parentNode = this.tree;
+    for (const key of path) {
+      if (parentNode.__listeners) {
+        parentNode.__listeners.forEach((cb) => cb(Date.now()));
+      }
+      parentNode = parentNode[key];
+    }
+  }
+}
+
+export const useData = (initialData) => {
+  const data = useRef(initialData).current;
+  const listeners = useRef(new ListenerTree());
+
+  const setData = (valueOrFunction) => {
+    if (typeof valueOrFunction === "function") {
+      data = valueOrFunction(data);
+    } else {
+      data = valueOrFunction;
+    }
+    listeners.current.notify([]);
+  };
+
+  const setDataWithSelector = (selector, newValue) => {
+    dataSetter(data, selector, newValue);
+    listeners.current.notify(selector.path);
+  };
+
+  const addListener = (selector, callback) => {
+    listeners.current.add(selector.path, callback);
+  };
+
+  const removeListener = (selector, callback) => {
+    listeners.current.remove(selector.path, callback);
+  };
+
+  return {
+    current: data,
+    setData,
+    segment: {
+      setDataWithSelector,
+      addListener,
+      removeListener,
+    },
+  };
+};
+
+export const useSegment = (state, path, options = {}) => {
+  const { detach = false } = options;
+  const selector = useRef(createSelector(path)).current;
+  const [data, setData] = useState(() => selector(state.current));
+
+  useEffect(() => {
+    const updateListener = () => {
+      if (!detach) {
+        setData(selector(state.current));
+      }
+    };
+
+    state.segment.addListener(selector, updateListener);
+    return () => {
+      state.segment.removeListener(selector, updateListener);
+    };
+  }, []);
+
+  const setter = (newValue) => {
+    setData(newValue);
+    state.segment.setDataWithSelector(selector, newValue);
+  };
+
+  return [data, setter];
+};
