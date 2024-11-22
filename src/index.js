c3943fb50ebf2566043cde1fc01b9521ae69f0bb
tycrockett <ty.crockett53@gmail.com>
2024-11-22 14:39:34 -0700

    import callback -- woops

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const createSelector = (path) => {
   const selector = (obj) => path.reduce((acc, key) => acc?.[key], obj);
